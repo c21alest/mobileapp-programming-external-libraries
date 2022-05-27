f9fe8e4f57012d9073b3b8315b6352eb0619c399
c21alest <alexsanderstrom@outlook.com>
2022-05-27 16:28:08 +0200

    OnClickListener för båda knapparna

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "==>";
+
+    private Button funny;
+    private Button happy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        funny = findViewById(R.id.button_funny);
+        happy = findViewById(R.id.button_happy);
+
+        funny.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Funny");
+            }
+        });
+
+        happy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Happy");
+            }
+        });
+
     }
 }
